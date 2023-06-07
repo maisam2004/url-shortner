5acb5b05e2447b7949a8358d48cddf2300734557
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-06-07 13:34:25 +0100

    Add methods post and get

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,6 +11,17 @@ def home():
 @app.route("/about")
 def about():
     return "<h2>this is url shortner </h2>"
+
+
+# request.args["code"] for get info from request
+# code=request.args["code"] ONLY FOR GET REQUESTS
+# for posts methods code=request.form.get["code"]
+@app.route("/your_url", methods=["GET", "POST"])
+def your_url():
+    if request.method == "POST":
+        return render_template("your_url.html", code=request.form.get("code"))
+    else:
+        return "This is not valid "
 
 
 if __name__ == "__main__":
