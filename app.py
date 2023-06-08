a722f606c20acb5ce282c7e0d4aa5528acc874a3
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-06-08 11:36:46 +0100

    Create json file and check exist var

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+import os.path
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", name="<h2>Mais<h2>")  # passing variable to page
+    return render_template("home.html", name="Mais")  # passing variable to page
 
 
 @app.route("/about")
@@ -18,16 +20,31 @@ def about():
 # for posts methods code=request.form["code"]
 # for posts methods code=request.form.get("code")
 @app.route("/yoururl", methods=["GET", "POST"])
-def your_url():
-    if request.method == "POST":
-        return render_template("yoururl.html", code=request.form["code"])
-    else:
-        return redirect(url_for("home"))  # redirect to function
+def your_url():  # sourcery skip: merge-dict-assign
+    if request.method != "POST":
+        return redirect(url_for("home"))  # redirect with url_for should give function
+    url = {}
+    if os.path.exists("ursls.json"):
+        with open("ursls.json") as jfile:
+            url = json.load(jfile)
+
+    if request.form["code"] in url.keys():
+        return redirect(url_for("home"))
+
+    url[request.form["code"]] = {"url": request.form["url"]}
+    with open("ursls.json", "w") as jfile:
+        json.dump(url, jfile)
+
+    return render_template(
+        "yoururl.html", code=request.form["code"], whole=request.form
+    )
+    # but if it is only redirect need to giv app.route slash item>> '/'
 
 
 # redirect to rout app
 # redirect("/")  redirect to rout app
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 # python app.py
